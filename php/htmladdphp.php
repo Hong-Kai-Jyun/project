c7344623da8d0467750e70631f542a8dba5c2061
try <s99101219@stu.edu.tw>
2019-12-12 21:05:25 +0800

    ..

--- php/htmladdphp.php
+++ php/htmladdphp.php
@@ -178,7 +178,19 @@ print "</table>";
   echo name();
   $backscore = score(100);
   echo "　成績等級：$backscore";
+
+  function sum(...$numbers){
+    $total = 0;
+    foreach ($numbers as $i){
+        $total += $i;
+    }
+    return $total;
+}
+
+echo "總共是：".sum(1,2,3);
+
+
 ?>
-  ?>
+
 </body>
 </html>
