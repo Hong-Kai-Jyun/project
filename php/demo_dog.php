7f0293362504a77901ec32bfe2430f615e11dd83
try <s99101219@stu.edu.tw>
2019-12-17 21:57:48 +0800

    .

--- php/demo_dog.php
+++ php/demo_dog.php
@@ -8,10 +8,11 @@ $myDog->dogBark();
 echo "狗狗的名子是 : ".$myDog->name."</br>";
 */
 include "minidog.php";
-$mydog2_s = new minidog2;
+/*
+$mydog2_s = new minidog2();
 echo "使用手機連線狗狗定位: ".$mydog2_s->seach()."</br>";
 
-
+*/
 
 $mydog2 = new minidog("小月月","灰色","二哈混柴犬","0.2");
 
