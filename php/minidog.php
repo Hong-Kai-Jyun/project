7129f9a59a4c8bae32a2954d3737ab5545c7a587
try <s99101219@stu.edu.tw>
2019-12-17 20:28:57 +0800

    ...

---
+++ php/minidog.php
@@ -0,0 +1,23 @@
+<?php
+
+include "dog2.php";
+
+class minidog extends dog{
+    protected $age;
+
+    public function __construct($name,$color,$style,age){
+
+        parent::_-construct($name,$color,$style);
+        $this->age = $age;
+    }
+
+    public function dowork(){
+        echo "吃~~~~~~~";
+    }
+
+    public function __destruct(){
+        echo "吃飽就睡了...(汗";
+    }
+}
+
+?>
