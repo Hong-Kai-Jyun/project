a4065d98fa78c4b832d008d624f0f2181db51082
try <s99101219@stu.edu.tw>
2019-12-17 20:42:13 +0800

    .

--- php/demo_dog.php
+++ php/demo_dog.php
@@ -10,6 +10,6 @@ echo "狗狗的名子是 : ".$myDog->$name."</br>";
 include "minidog.php";
 $mydog2 = new minidog("小月月","灰色","二哈混柴犬",0.2);
 echo "第二隻狗的名子叫 :".$mydog2->name."</br>";
-echo "他的顏色是 : ".$mydog2->colog."</br>";
+echo "他的顏色是 : ".$mydog2->color."</br>";
 echo "他的品種 : ".$mydog2->style."</br>";
 echo "讓他去~~~~".$mydog2->dowork()."</br>之後就... ";
