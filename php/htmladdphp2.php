11f4157e8b4907cff3aa2624443ba73c786631c9
try <s99101219@stu.edu.tw>
2019-12-17 19:50:03 +0800

    ..

--- php/htmladdphp2.php
+++ php/htmladdphp2.php
@@ -6,16 +6,16 @@
      private $z = "kkk";
 
      public function getpara(){
-         echo "inclass->x:".$this->x."</br>";
-         echo "inclass->y".$this->y."</br>";
-         echo "inclass->z".$this->z."</br>";
+         echo "inclass->x : ".$this->x."</br>";
+         echo "inclass->y : ".$this->y."</br>";
+         echo "inclass->z : ".$this->z."</br>";
      }
  }
 
  $test = new mycalss;
  $test->getpara();
- echo "test->x:".$test->x."</br>";
- echo "test->y:".$test->y."</br>";
- echo "test->z".$test->z."</br>";
+ echo "test->x : ".$test->x."</br>";
+ echo "test->y : ".$test->y."</br>";
+ echo "test->z : ".$test->z."</br>";
 
 ?>
