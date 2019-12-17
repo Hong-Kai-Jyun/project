8a53efa488be1f9d5aad2f681515d3ab04059010
try <s99101219@stu.edu.tw>
2019-12-17 20:48:24 +0800

    .

--- php/demo_dog.php
+++ php/demo_dog.php
@@ -1,12 +1,12 @@
 <?php
-/*
+
 include "dog2.php";
 
 $myDog = new dog("月月","棕色","柴犬");
 $myDog->dogRun();
 $myDog->dogBark();
-echo "狗狗的名子是 : ".$myDog->$name."</br>";
-*/
+echo "狗狗的名子是 : ".$myDog->name."</br>";
+
 include "minidog.php";
 $mydog2 = new minidog("小月月","灰色","二哈混柴犬",0.2);
 echo "第二隻狗的名子叫 :".$mydog2->name."</br>";
