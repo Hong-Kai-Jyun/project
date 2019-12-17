2c748061e4245a5120da699a1e769a16a84105db
try <s99101219@stu.edu.tw>
2019-12-17 19:46:05 +0800

    .

---
+++ php/htmladdphp2.php
@@ -0,0 +1,21 @@
+<?php
+
+ class mycalss{
+     public $x = 1234;
+     protected $y ="Spring";
+     private $z = "kkk";
+
+     public function getpara(){
+         echo "inclass->x:".$this->x."</br>";
+         echo "inclass->y".$this->y."</br>";
+         echo "inclass->z".$this->z."</br>";
+     }
+ }
+
+ $test = new myclass;
+ $test->getpara();
+ echo "test->x:".$test->x."</br>";
+ echo "test->y:".$test->y."</br>";
+ echo "test->z".$test->z."</br>";
+
+?>
