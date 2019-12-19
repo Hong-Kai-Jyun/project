045b13cfbe5b8ac3be1e2efc29d8aa5b5e97585a
try <s99101219@stu.edu.tw>
2019-12-19 21:23:48 +0800

    .

---
+++ php/Person.php
@@ -0,0 +1,34 @@
+<?php
+
+ class Person{
+     private $yell;
+     public function __set($name, $value){
+
+      if(isset($this->$name)){
+           return $thsi->$name = $value;
+      } else {
+          return null;
+      }
+     }
+
+
+     public function  __get($name){
+         return $name;
+     }
+
+     public function __isset($name){
+         return $name;
+     }
+
+     public function __unset($name){
+         return $name;
+     } 
+}
+
+$person = new Person();
+$person->name = 'PHP';
+echo $person->name.PHP_EOL;
+echo $person->sex.PHP_EOL;
+echo isset($person->address);
+unset($person->name);
+?>
