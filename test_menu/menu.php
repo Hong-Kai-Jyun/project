dec8af17b119231a9f977dc32d8623b06e12f462
Hong-Kai-Jyun <s99101219@stu.edu.tw>
2019-12-22 14:53:30 +0800

    .

--- test_menu/menu.php
+++ test_menu/menu.php
@@ -9,12 +9,13 @@
  <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css"><!--匯入bootstrap-->
  <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script><!--匯入jQuery-->
  <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/js/bootstrap.min.js"></script><!--匯入bootstrap javascript-->
-
+ 
  <style type="text/css">
 		    .a{
 				height: 25vh;
+                overflow:auto;
 			}
- </style>
+ </style><!--固定菜單-->
 </head>
 
 <body>
