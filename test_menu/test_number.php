564173e4f36cb4a57aecf5efe4c14bc0ae508e83
Hong-Kai-Jyun <s99101219@stu.edu.tw>
2019-12-22 16:15:29 +0800

    .

--- test_menu/test_number.php
+++ test_menu/test_number.php
@@ -1,8 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    
+</body>
+</html>
 <?php
 $Number=array('1','2','3','+','4','5','6','-','7','8','9','*','清除','0','x');
 $i=count($Number);
 for($j=0 ; $j<$i ; $j++){
-    echo '第 '.$j.' 圈 : '.$Number[$j] ;
+    echo "<button>".$Number[$j]."</button>" ;
 //　echo '第 '.$j.' 圈 : '.$Number[$j] ;
 //echo $Number[$j]/4;
  if($Number[$j] / 4 == 0)
