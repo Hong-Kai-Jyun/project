e0903fa83e903fc891cf5e425487a7ad670de334
Hong-Kai-Jyun <s99101219@stu.edu.tw>
2019-12-22 13:56:05 +0800

    ..

--- test_menu/menu.php
+++ test_menu/menu.php
@@ -12,49 +12,55 @@
 </head>
 
 <body>
- <ul class="nav nav-tabs">
- <li><a data-toggle="tab" href="#home">紅茶系列</a></li>
- <li><a data-toggle="tab" href="#menu1">冬瓜系列</a></li>
- <li><a data-toggle="tab" href="#menu2">仙草系列</a></li>
- <li class="dropdown"><a class="dropdown-toggle" data-toggle="dropdown" href="#">Dropdown<span class="caret"></span></a>
-     <ul class="dropdown-menu">
-         <li><a href="#">First</a></li>
-         <li><a href="#">Second</a></li>
-         <li><a href="#">Third</a></li>
-     </ul>
- </li>
- </ul>
+<div class="nav navbar-default navbar-fixed-bottom">
+  <ul class="nav nav-tabs">
+   <li><a data-toggle="tab" href="#home">紅茶系列</a></li>
+   <li><a data-toggle="tab" href="#menu1">冬瓜系列</a></li>
+   <li><a data-toggle="tab" href="#menu2">仙草系列</a></li>
+   <li class="dropdown"><a class="dropdown-toggle" data-toggle="dropdown" href="#">Dropdown<span class="caret"></span></a>
+       <ul class="dropdown-menu">
+           <li><a href="#">First</a></li>
+           <li><a href="#">Second</a></li>
+           <li><a href="#">Third</a></li>
+       </ul>
+   </li>
+  </ul>
 
- <div class="tab-content">
- <div id="home" class="tab-pane fade in active">
-     <button>紅茶冰</button>
-     <button>檸檬紅茶</button>
-     <p>金桔紅茶</p>
-     <p>紅茶豆漿</p>
-     <p>紅茶多多</p>
-     <p>紅茶牛奶</p>
-     <p>特濃紅茶牛奶</p>
-     <p>特濃紅茶豆漿</p>
+  <div class="tab-content">
+   <div id="home" class="tab-pane fade in active">
+       <button>紅茶冰</button>
+       <button>檸檬紅茶</button>
+       <p>金桔紅茶</p>
+       <p>紅茶豆漿</p>
+       <p>紅茶多多</p>
+       <p>紅茶牛奶</p>
+       <p>特濃紅茶牛奶</p>
+       <p>特濃紅茶豆漿</p>
+   </div>
+
+   <div id="menu1" class="tab-pane fade">
+       <p>冬瓜冰</p>
+       <p>冬瓜紅茶</p>
+       <p>檸檬冬瓜</p>
+       <p>金桔冬瓜</p>
+       <p>冬瓜豆漿</p>
+       <p>冬瓜多多</p>
+       <p>冬瓜牛奶</p>
+       <p>特濃冬瓜牛奶</p>
+       <p>特濃冬瓜豆漿</p>
+   </div>
+
+   <div id="menu2" class="tab-pane fade">
+       <p>麥仔茶</p>
+       <p>麥香紅茶</p>
+       <p>麥茶冬瓜</p>
+       <p>麥茶豆漿</p>
+       <p>麥茶牛奶</p>
+       <p>特濃麥茶豆漿</p>
+       <p>特濃麥茶牛奶</p>
+   </div>
+  </div>
+
  </div>
- <div id="menu1" class="tab-pane fade">
-     <p>冬瓜冰</p>
-     <p>冬瓜紅茶</p>
-     <p>檸檬冬瓜</p>
-     <p>金桔冬瓜</p>
-     <p>冬瓜豆漿</p>
-     <p>冬瓜多多</p>
-     <p>冬瓜牛奶</p>
-     <p>特濃冬瓜牛奶</p>
-     <p>特濃冬瓜豆漿</p>
- </div>
- <div id="menu2" class="tab-pane fade">
-     <p>麥仔茶</p>
-     <p>麥香紅茶</p>
-     <p>麥茶冬瓜</p>
-     <p>麥茶豆漿</p>
-     <p>麥茶牛奶</p>
-     <p>特濃麥茶豆漿</p>
-     <p>特濃麥茶牛奶</p>
- </div>
- </div>
+ 
  </body>
