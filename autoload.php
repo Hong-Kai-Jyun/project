e43232fafe16d0792dfcc345ae2b4a8bada870e0
try <s99101219@stu.edu.tw>
2019-12-26 20:09:34 +0800

    ..

--- autoload.php
+++ autoload.php
@@ -1,7 +1,7 @@
 <?php
 
 function __autoload($classname){
-    $file_name = ___DIR___."/Classes/".$classname.".php";
+    $file_name = __DIR__."/Classes/".$classname.".php";
     if(is_readable($file_name)){
          require $file_name;
     }
