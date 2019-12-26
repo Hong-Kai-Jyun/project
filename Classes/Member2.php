693001255bd4ca314099d38142af00cfa48c67c7
try <s99101219@stu.edu.tw>
2019-12-26 22:09:44 +0800

    .

--- Classes/Member2.php
+++ Classes/Member2.php
@@ -7,6 +7,20 @@
     public function getMemberList(){
       echo "好吃好吃!";
   }
+ /*
+  class MEMBER{
+      public function getMEMBERLIST(){
+          echo "好吃好吃!";
+      }
 
+      public function getMemberList(){
+        echo "好吃好吃!";
+    }
+  }
+ 
+  class MEMBER2_2{
+    public function getMEMBERLIST(){
+        echo "好吃好吃!";
+    }*/
 }
 ?>
