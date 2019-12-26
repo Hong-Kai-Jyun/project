9847f2246876446c86c5eaafd70a436a35a02568
try <s99101219@stu.edu.tw>
2019-12-26 21:56:01 +0800

    .

--- index2.php
+++ index2.php
@@ -5,7 +5,7 @@ include_once __DIR__."/autoload2.php";
 $member = new Member();
 $member->getMemberList();*/
 
-$MEMBER = new MEMBER();
-$MEMBER->getMEMBERLIST();
+$member = new MEMBER();
+$member->getMEMBERLIST();
 
 ?>
