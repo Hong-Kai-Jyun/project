52616eba59db0caf4e339a4e591ad8e02a9a7f91
try <s99101219@stu.edu.tw>
2019-12-26 21:36:10 +0800

    .

--- autoload2.php
+++ autoload2.php
@@ -3,10 +3,10 @@ function __autoload($className){
   $file_name = __DIR__ . "/Classes/" . $className . ".php";
   echo "file_name:".$file_name."</br>";
     echo "classname:".$className."/br";
-/*
+
     if(is_readable($file_name)){
          require $file_name;
-    }*/
+    }
 }
 
 ?>
