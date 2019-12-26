ed7f57e52401d944227665e74cc81c5b512944a5
try <s99101219@stu.edu.tw>
2019-12-26 22:01:42 +0800

    .

--- Classes/Member2.php
+++ Classes/Member2.php
@@ -1,5 +1,13 @@
 <?php
+  class Member2{
+    public function getMEMBERLIST(){
+        echo "好吃好吃!";
+    }
 
+    public function getMemberList(){
+      echo "好吃好吃!";
+  }
+}
   class MEMBER{
       public function getMEMBERLIST(){
           echo "好吃好吃!";
